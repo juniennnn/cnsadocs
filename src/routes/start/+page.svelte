41d94d5bd26e11537c5d402c129a30e08afc0f2b
anear141421356 <anear141421@gmail.com>
2026-06-17 17:52:37 +0900

    .

--- src/routes/start/+page.svelte
+++ src/routes/start/+page.svelte
@@ -5,21 +5,28 @@
     let { data, form }: PageProps = $props();
 </script>
 
-<h1>아이디를 입력해주세요</h1>
-<p>{data.user.email}</p>
-
-<form method="POST" use:enhance>
-    <label for="name">아이디 (2~10자)</label>
-    <input
-        id="name"
-        name="name"
-        type="text"
-        minlength="2"
-        maxlength="10"
-        required
-    />
+<div class="mx-auto text-center items-center gap-10">
+    <br class="my-10">
+    <p class="text-xl text-left mx-9">{data.user.email}</p><br>
+    <h1 class="text-left mx-9">아이디를 입력해주세요:</h1><br>
+    <form method="POST" use:enhance>
+       <div>
+            <label for="name">아이디 (2~10자)</label>
+                <input
+                id="name"
+                name="name"
+                type="text"
+                minlength="2"
+                maxlength="10"
+                required
+                class="border-0 bg-amber-200 rounded-xl"
+            />
+        </div> 
+    </form>
+    <br class="my-10">
+    <button class="px-4 py-2 bg-amber-300 rounded-xl" type="submit">시작하기</button>
+</div>
     {#if form?.error}
         <p>{form.error}</p>
     {/if}
-    <button type="submit">시작하기</button>
-</form>
+
